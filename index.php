edbc66c9a0b777b29dccae5f81b8025ad8f9cd72
hedii <hedi.chaibs@gmail.com>
2015-07-06 10:53:35 +0200

    fix utf8 encoding

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 use ColissimoApi\ColissimoApi;
 
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 require 'ColissimoApi/ColissimoApi.php';
 $colissimoApi = new ColissimoApi();
 $colissimoApi->run();
